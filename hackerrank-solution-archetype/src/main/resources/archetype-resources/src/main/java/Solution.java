73f246bd32e873f9a25476e75a280054c376bd8e
Marcelo de Moraes Leite <marcelo.leite@cwi.com.br>
2019-09-11 08:50:31 -0300

    Create new archetype for HackerRank solutions

---
+++ hackerrank-solution-archetype/src/main/resources/archetype-resources/src/main/java/Solution.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+/**
+ * Paste in this file the problem class skeleton.
+ */
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+    }
+}
